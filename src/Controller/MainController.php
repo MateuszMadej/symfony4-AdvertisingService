7650252f81d76f66c3926008b0ede912048d9421
Mateusz Madej <madeej1@hotmail.com>
2019-10-25 13:01:23 +0200

    Added new routes and templates.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,17 +4,51 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use \DateTime;
 
 class MainController extends AbstractController
 {
     /**
-     * @Route("/main", name="main")
+     * @Route("/", name="index")
      */
     public function index()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MainController.php',
-        ]);
+        return $this->render('main/index.html.twig');
+    }
+
+    /**
+     * @Route("/registration", name="registration")
+     */
+    public function registration()
+    {
+        return $this->render('main/registration.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login()
+    {
+        return $this->render('main/login.html.twig');
+    }
+
+    /**
+     * @Route("/logout", name="logout")
+     */
+    public function logout(SessionInterface $session)
+    {
+        // remove session here
+        return $this->redirectToRoute('index');
+    }
+
+    /**
+     * @Route("/profile", name="profile")
+     */
+    public function profile()
+    {
+        return $this->render('main/profile.html.twig');
     }
 }
